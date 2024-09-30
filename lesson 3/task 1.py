381e31969f95636481137a069b8a0764e66085c8
3m0k1d <emot1onaldis0rderz@gmail.com>
2024-09-30 22:12:37 +0300

    add file

--- lesson 3/task 1.py
+++ lesson 3/task 1.py
@@ -10,5 +10,10 @@ print(x)
 
 for x in range(1,10):
     for i in range(1,10):
-        print(f'{x} x {i} = {x*i}')
+        print(("{:3d}".format(x * i,)), end='')
+
     print()
+
+m = [[1, 2, 3], [2, 3, 4], [6, 4, 7]]
+matr = [[m[j][i] for j in range(len(m))] for i in range(len(m[0])-1,-1,-1)]
+print(matr)
