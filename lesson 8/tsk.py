61c62a088d4a9d44e7a58238208a004b5a9cbab0
3m0k1d <emot1onaldis0rderz@gmail.com>
2024-12-01 20:29:36 +0300

    add file

--- lesson 8/tsk.py
+++ lesson 8/tsk.py
@@ -12,3 +12,24 @@ assert f("С227НА777") == "Private"
 assert f("КУ22777") == "Taxi"
 assert f("Т22В7477") == "Fail"
 assert f("М227К19У9") == "Fail"
+
+
+
+from functools import reduce
+file = open('hometask8.txt', 'r', encoding = 'utf-8')
+s = file.readlines()
+words = reduce(lambda x,y: x[:-1]+' '+y,s).split()
+print(words)
+print(list(filter(lambda x: re.fullmatch(r'\b[А-Я]+[-]?[А-Я]+\b',x))))
+
+s = 'Уважаемые! Если вы к 09:00 не вернете чемодан, то уже в 09:00:01 я за себя не отвечаю.'
+s = s.split()
+x = list(filter(lambda x: re.search(r'\b(0[0-9]│1[0-9]│2[0-4])[:](0[0-9]│[0-9]{2}\b', x), s))
+s = reduce(lambda x,y: x+' '+y, s)
+for i in x:
+    s = s.replace(i,'(TBD)',1)
+print(s)
+
+s = 'Владисир устроился на работу'
+x = re.findall((r'\b[А-Я][А-Я]*[А-Я]\b', s))
+print(x)
